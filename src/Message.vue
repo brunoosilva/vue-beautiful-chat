c48051dbeac892a36abcc03315f7f62b8319cdb0
Bruno Oliveira <321.bruno@gmail.com>
2019-10-15 23:20:00 -0300

    Add audio message

--- src/Message.vue
+++ src/Message.vue
@@ -36,6 +36,7 @@
           <slot name="system-message-body" :message="message.data">
           </slot>
       </SystemMessage>
+      <AudioMessage v-else-if="message.type === 'audio'" :data="message.data" />
     </div>
   </div>
 </template>
